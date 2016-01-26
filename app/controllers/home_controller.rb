444506d1fc4071028c70a22f6b6a406fd3c756e7
iulia <iulia@indigenious.ro>
2016-01-26 12:08:35 +0200

    removed unnecessary code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  skip_before_filter :check_buyer, :only => :index
+
+
   def index
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-
+  before_filter :check_buyer
 
 
   def index
@@ -11,9 +11,19 @@ class ProductsController < ApplicationController
   def buy_product
     @variant = Variant.where(id: params[:variant_id]).first
     @product = @variant.product
-    @buyer = current_user
+    @buyer = current_buyer
     @variant.update_attributes(quantity: (@variant.quantity-1)) if @buyer && @buyer.credit >= @variant.price
     Coupon.create(variant_id: @variant.id, code: Coupon.generate_new_code)
     redirect_to action: 'index'
   end
+
+
+  protected
+
+  def check_buyer
+    unless buyer_signed_in?
+      redirect_to '/'
+    end
+  end
+
 end
