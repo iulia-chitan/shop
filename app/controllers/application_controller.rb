64cf275550ace2771c16fe5e25e6b355eaea1dcd
iulia <iulia@indigenious.ro>
2016-01-26 11:54:36 +0200

    products listing and buy product only if buyer is logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_buyer
+
+
+  helper_method :current_user
+
+
+  private
+
+  def current_user
+    @current_user ||= Buyer.find(session[:buyer_id]) if session[:buyer_id]
+  end
+
+  protected
+
+  def check_buyer
+    if current_user.nil?
+      redirect_to '/'
+    end
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+
 
 
   def index
